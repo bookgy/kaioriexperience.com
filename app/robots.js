46e71e86fa61a3279c9ec146ee582d09bb8dd410
Carlos Barquín <info@bookgy.com>
2026-06-03 20:00:19 +0200

    Actualiza web

--- app/robots.js
+++ app/robots.js
@@ -1,5 +1,7 @@
 import { siteUrl } from "../src/data/seo";
 
+export const dynamic = "force-static";
+
 export default function robots() {
   return {
     rules: {
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,5 +1,7 @@
 import { indexedRoutes, siteUrl } from "../src/data/seo";
 
+export const dynamic = "force-static";
+
 export default function sitemap() {
   const lastModified = new Date();
 
